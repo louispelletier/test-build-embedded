8e783079e816f2b569eabbd68b7258b2d606164e
OPetitLordPet <opetit@hotmail.fr>
2021-12-28 20:47:24 -0500

    (git action): add the cppu test

---
+++ tests/tests/MyFirstTest.cpp
@@ -0,0 +1,27 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C"
+{
+// #include "main.c"
+}
+
+TEST_GROUP(MyCode)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+};
+
+TEST(MyCode, test1)
+{
+   FAIL("Your test is running! Now delete this line and watch your test pass.");
+    /*
+     * Instantiate your class, or call the function, you want to test.
+     * Then delete this comment
+     */
+}
+
